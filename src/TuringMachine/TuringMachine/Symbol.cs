231caac1fb36dc92982146f79ff80b8707cc6518
Bokros Ferenc <bokros.feri@hotmail.com>
2020-11-22 22:43:52 +0100

    Symbol: renaming constant HashCodeIfValueIsNull -> NullValueHashCode

--- src/TuringMachine/TuringMachine/Symbol.cs
+++ src/TuringMachine/TuringMachine/Symbol.cs
@@ -8,7 +8,7 @@ namespace TuringMachine
     /// <typeparam name="T">Typeof of the symbolised data.</typeparam>
     public sealed class Symbol<T>
     {
-        private const int HashCodeIfValueIsNull = 0;
+        private const int NullValueHashCode = 0;
         
         /// <summary>
         /// Gets the blank symbol tape value.
@@ -86,7 +86,7 @@ namespace TuringMachine
             }
             else if (Value == null)
             {
-                return HashCodeIfValueIsNull;
+                return NullValueHashCode;
             }
             else
             {
