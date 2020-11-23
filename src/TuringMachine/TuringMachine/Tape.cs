cf6f7f5fa43daa7328778cb6ce326b17bf755207
Bokros Ferenc <bokros.feri@hotmail.com>
2020-11-23 23:01:23 +0100

    sealed modifier added to Tape class.

---
+++ src/TuringMachine/TuringMachine/Tape.cs
@@ -0,0 +1,11 @@
+﻿namespace TuringMachine
+{
+    /// <summary>
+    /// Represents a Turing machine tape that reads and modifies the stored symbols.
+    /// </summary>
+    /// <typeparam name="T">Type of the symbols' data.</typeparam>
+    internal sealed class Tape<T>
+    {
+
+    }
+}
