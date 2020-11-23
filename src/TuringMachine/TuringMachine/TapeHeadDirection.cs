55f6b1581bae888f867df9c350ea0c3f10a7cb8f
Bokros Ferenc <bokros.feri@hotmail.com>
2020-11-23 22:57:28 +0100

    TapeHeadDirection enum added to TuringMachine project.

---
+++ src/TuringMachine/TuringMachine/TapeHeadDirection.cs
@@ -0,0 +1,23 @@
+﻿namespace TuringMachine
+{
+    /// <summary>
+    /// Movement direction of the tape's head.
+    /// </summary>
+    internal enum TapeHeadDirection
+    {
+        /// <summary>
+        /// Not moves anywhere.
+        /// </summary>
+        Stay = 1,
+
+        /// <summary>
+        /// Movement in the left direction.
+        /// </summary>
+        Left,
+
+        /// <summary>
+        /// Movement in the right direction.
+        /// </summary>
+        Right
+    }
+}
