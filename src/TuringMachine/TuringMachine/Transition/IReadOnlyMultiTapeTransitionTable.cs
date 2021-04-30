9e168c1aaaf2cd70f79db6973b05e8a98e6db885
Ferenc Bokros <ferenc.bokros@crystalclear.hu>
2021-04-30 21:45:42 +0200

    IReadOnlyMultiTapeTransitionTable interface introduced.

---
+++ src/TuringMachine/TuringMachine/Transition/IReadOnlyMultiTapeTransitionTable.cs
@@ -0,0 +1,18 @@
+﻿namespace TuringMachine.Transition
+{
+    /// <summary>
+    /// Defines operations for a readonly multi tape transition table.
+    /// </summary>
+    /// <typeparam name="TState">Type of the machine's state.</typeparam>
+    /// <typeparam name="TValue">Type of the symbolised data.</typeparam>
+    public interface IReadOnlyMultiTapeTransitionTable<TState, TValue>
+    {
+        /// <summary>
+        ///  Gets the range of a machine transition at the given tape that belongs to the given transition domain.
+        /// </summary>
+        /// <param name="tapeId">Identifier of the tape.</param>
+        /// <param name="domain">Domain of a multi tape machine transition.</param>
+        /// <returns><see cref="TransitionRange{TState, TValue}"/> that belongs to the given transition domain.</returns>
+        TransitionRange<TState, TValue> GetTransitionRangeAtTape(int tapeId, TransitionDomain<TState, TValue> domain);
+    }
+}
