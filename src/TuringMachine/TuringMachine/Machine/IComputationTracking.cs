965f1f24365c7c7eaa1feb62c7d682c298157ce8
Ferenc Bokros <ferenc.bokros@crystalclear.hu>
2021-09-09 23:47:07 +0200

    IComputationTracking interface added to project.

---
+++ src/TuringMachine/TuringMachine/Machine/IComputationTracking.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TuringMachine.Machine
+{
+    /// <summary>
+    /// Notifies clients that computation state has changed.
+    /// </summary>
+    /// <typeparam name="TState">Type of the machine's state.</typeparam>
+    /// <typeparam name="TSymbol">Type of the symbolised data.</typeparam>
+    public interface IComputationTracking<TState, TSymbol>
+    {
+        /// <summary>
+        /// Occures when a machine transitions from one state to another.
+        /// </summary>
+        event EventHandler<SteppedEventArgs<TState, TSymbol>> Stepped;
+
+        /// <summary>
+        /// Occures when a computation terminated.
+        /// </summary>
+        event EventHandler<ComputationTerminatedEventArgs<TState, TSymbol>> ComputationTerminated;
+    }
+}
