527109bac533e4a891a19770ce9acc8c1fd65270
Ferenc Bokros <ferenc.bokros@crystalclear.hu>
2021-09-20 00:01:34 +0200

    SingleTapeMachine class added to project and implements IComputationTracking interface.

---
+++ src/TuringMachine/TuringMachine/Machine/SingleTapeMachine.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using TuringMachine.Transition;
+
+namespace TuringMachine.Machine
+{
+    public class SingleTapeMachine<TState, TSymbol> : IComputationTracking<TState, TSymbol>
+    {
+        public event EventHandler<SteppedEventArgs<TState, TSymbol>>? Stepped;
+        public event EventHandler<ComputationTerminatedEventArgs<TState, TSymbol>>? ComputationTerminated;
+
+        private object computationLock;
+        private ComputationMode? computationMode;
+        private int stepCount;
+        private Tape<TSymbol> tape;
+        private TransitionDomain<TState, TSymbol>? configuration;
+        private TransitionTable<TState, TSymbol> transitionTable;
+
+        /// <summary>
+        /// Initializes a new instance of <see cref="SingleTapeMachine{TState, TSymbol}"/> class with the given transition table.
+        /// </summary>
+        /// <param name="transitionTable">Table that contains the performable transitions.</param>
+        public SingleTapeMachine(TransitionTable<TState, TSymbol> transitionTable)
+        {
+            computationLock = new object();
+            tape = new Tape<TSymbol>();
+            this.transitionTable = transitionTable;
+        }
+    }
+}
