32793557eece9cef165f2388a4df95499aba910c
Ferenc Bokros <ferenc.bokros@crystalclear.hu>
2021-09-21 21:02:31 +0200

    IComputationConstraint interface added to project.

---
+++ src/TuringMachine/TuringMachine/Machine/ComputationConstraint/IComputationConstraint.cs
@@ -0,0 +1,16 @@
+﻿namespace TuringMachine.Machine.ComputationConstraint
+{
+    /// <summary>
+    /// Defines rules that must be enforced during a computation.
+    /// </summary>
+    /// <typeparam name="TState">Type of the machine's state.</typeparam>
+    /// <typeparam name="TSymbol">Type of the symbolised data.</typeparam>
+    public interface IComputationConstraint<TState, TSymbol>
+    {
+        /// <summary>
+        /// Checks whether the constraint is enforced.
+        /// </summary>
+        /// <param name="computationState">Computation state that the rules apply to.</param>
+        void Enforce(ComputationState<TState, TSymbol> computationState);
+    }
+}
