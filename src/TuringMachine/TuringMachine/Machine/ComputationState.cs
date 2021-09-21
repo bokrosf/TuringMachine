c2729773743f52366f51727a20749b9ea3077dbf
Ferenc Bokros <ferenc.bokros@crystalclear.hu>
2021-09-21 20:45:56 +0200

    ComputationState record added to project.

---
+++ src/TuringMachine/TuringMachine/Machine/ComputationState.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using TuringMachine.Transition;
+
+namespace TuringMachine.Machine
+{
+    /// <summary>
+    /// Represents the state of a computation.
+    /// </summary>
+    /// <typeparam name="TState">Type of the machine's state.</typeparam>
+    /// <typeparam name="TSymbol">Type of the symbolised data.</typeparam>
+    public record ComputationState<TState, TSymbol>(TransitionDomain<TState, TSymbol> Configuration, int StepCount, TimeSpan ElapsedTime);
+}
