aaf6653144cfb1f7361d3131c42cbac78380f9a2
Ferenc Bokros <ferenc.bokros@crystalclear.hu>
2021-09-26 22:19:21 +0200

    MultiConstraint class added to project.

---
+++ src/TuringMachine/TuringMachine/Machine/ComputationConstraint/MultiConstraint.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TuringMachine.Machine.ComputationConstraint
+{
+    /// <summary>
+    /// Ensures all the given constraints.
+    /// </summary>
+    /// <typeparam name="TState">Type of the machine's state.</typeparam>
+    /// <typeparam name="TSymbol">Type of the symbolised data.</typeparam>
+    public class MultiConstraint<TState, TSymbol> : ComputationConstraint<TState, TSymbol>
+    {
+        private readonly IReadOnlyCollection<ComputationConstraint<TState, TSymbol>> constraints;
+
+        /// <summary>
+        /// Initializes a new instance of <see cref="MultiConstraint{TState, TSymbol}"/> class with the specified constraints.
+        /// </summary>
+        /// <param name="constraints">Constraints need to be enforced.</param>
+        public MultiConstraint(IEnumerable<ComputationConstraint<TState, TSymbol>> constraints)
+        {
+            if (!constraints.Any())
+            {
+                throw new ArgumentException("Collection must contain at least one constraint.", nameof(constraints));
+            }
+
+            this.constraints = constraints.ToList().AsReadOnly();
+        }
+
+        /// <inheritdoc/>
+        /// <exception cref="ComputationAbortedException">Any of the constraints could not be enforced.</exception>
+        public override void Enforce(IReadOnlyComputationState<TState, TSymbol> computationState)
+        {
+            if (IsComputationFinished(computationState))
+            {
+                return;
+            }
+
+            foreach (var c in constraints)
+            {
+                c.Enforce(computationState);
+            }
+        }
+    }
+}
