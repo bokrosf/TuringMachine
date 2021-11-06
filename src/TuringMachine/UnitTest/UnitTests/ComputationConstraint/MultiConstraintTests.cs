8da95eecf8bf24ea7a3d514e07c72fc57e2c8c92
Ferenc Bokros <ferenc.bokros@crystalclear.hu>
2021-11-06 18:09:15 +0100

    MultiConstraint tests added to Tests project. Nullable warnings threated as errors corrected.

---
+++ src/TuringMachine/UnitTest/UnitTests/ComputationConstraint/MultiConstraintTests.cs
@@ -0,0 +1,81 @@
+﻿using Moq;
+using System;
+using System.Linq;
+using TuringMachine.Machine.Computation;
+using TuringMachine.Machine.Computation.Constraint;
+using Xunit;
+
+namespace TuringMachine.Tests.UnitTests.ComputationConstraint
+{
+    public class MultiConstraintTests
+    {
+        [Fact]
+        public void Constructor_EmptyConstraintList_ThrowsException()
+        {
+            var contraints = Enumerable.Empty<IComputationConstraint<int, char>>();
+            Assert.Throws<ArgumentException>(() => new MultiConstraint<int, char>(contraints));
+        }
+
+        [Fact]
+        public void Enforce_SingleValidConstraint_NotThrowsException()
+        {
+            var mockComputationState = new Mock<IReadOnlyComputationState<int, char>>();
+            var mockSingleConstraint = new Mock<IComputationConstraint<int, char>>();
+            var constraints = new IComputationConstraint<int, char>[] { mockSingleConstraint.Object };
+            var multiConstraint = new MultiConstraint<int, char>(constraints);
+
+            multiConstraint.Enforce(mockComputationState.Object);
+        }
+
+        [Fact]
+        public void Enforce_AllConstraintsValid_NotThrowsException()
+        {
+            var mockComputationState = new Mock<IReadOnlyComputationState<int, char>>();
+            var constraints = Enumerable.Range(0, 5).Select(i => new Mock<IComputationConstraint<int, char>>().Object);
+            var multiConstraint = new MultiConstraint<int, char>(constraints);
+
+            multiConstraint.Enforce(mockComputationState.Object);
+        }
+
+        [Fact]
+        public void Enforce_SingleInvalidConstraint_ThrowsException()
+        {
+            var mockComputationState = new Mock<IReadOnlyComputationState<int, char>>();
+            var constraints = new IComputationConstraint<int, char>[] { CreateInvalidConstraintMock<int, char>() };
+            var multiConstraint = new MultiConstraint<int, char>(constraints);
+
+            Assert.Throws<ComputationAbortedException>(() => multiConstraint.Enforce(mockComputationState.Object));
+        }
+
+        [Fact]
+        public void Enforce_AllConstraintsInvalid_ThrowsException()
+        {
+            var mockComputationState = new Mock<IReadOnlyComputationState<int, char>>();
+            var constraints = Enumerable.Range(0, 5).Select(i => CreateInvalidConstraintMock<int, char>());
+            var multiConstraint = new MultiConstraint<int, char>(constraints);
+
+            Assert.Throws<ComputationAbortedException>(() => multiConstraint.Enforce(mockComputationState.Object));
+        }
+
+        [Fact]
+        public void Enforce_MixedValidityConstraints_ThrowsException()
+        {
+            var mockComputationState = new Mock<IReadOnlyComputationState<int, char>>();
+            var validConstraints = Enumerable.Range(0, 5).Select(i => new Mock<IComputationConstraint<int, char>>().Object);
+            var invalidConstraints = Enumerable.Range(0, 5).Select(i => CreateInvalidConstraintMock<int, char>());
+            var multiConstraint = new MultiConstraint<int, char>(validConstraints.Concat(invalidConstraints));
+
+            Assert.Throws<ComputationAbortedException>(() => multiConstraint.Enforce(mockComputationState.Object));
+        }
+
+        private IComputationConstraint<TState, TSymbol> CreateInvalidConstraintMock<TState, TSymbol>()
+        {
+            var constraint = new Mock<IComputationConstraint<TState, TSymbol>>();
+            constraint
+                .Setup(sc => sc.Enforce(It.IsAny<IReadOnlyComputationState<TState, TSymbol>>()))
+                .Throws<ComputationAbortedException>();
+
+            return constraint.Object;
+        }
+    }
+}
