261103371dcf71736973d907f9ec42010f0be7c1
Ferenc Bokros <ferenc.bokros@crystalclear.hu>
2021-11-08 21:56:17 +0100

    Asynchronous versions of StartAutomaticComputation_WithoutConstraint_SteppedRaised and StartAutomaticComputation_WithoutConstraint_ComputationTerminatedRaised tests have been added.

--- src/TuringMachine/UnitTest/UnitTests/Machine/SingleTape/AutomaticComputationTests.cs
+++ src/TuringMachine/UnitTest/UnitTests/Machine/SingleTape/AutomaticComputationTests.cs
@@ -1,4 +1,5 @@
-﻿using TuringMachine.Machine;
+﻿using System.Threading.Tasks;
+using TuringMachine.Machine;
 using TuringMachine.Machine.Computation;
 using Xunit;
 
@@ -31,6 +32,36 @@ namespace TuringMachine.Tests.UnitTests.Machine.SingleTape
                 handler => machine.ComputationTerminated += handler,
                 handler => machine.ComputationTerminated -= handler,
                 () => machine.StartAutomaticComputation(arguments.Input));
+
+            Assert.Same(machine, raisedStepped.Sender);
+            Assert.Equal(State<int>.Accept, raisedStepped.Arguments.State);
+        }
+
+        [Theory]
+        [ClassData(typeof(AcceptTerminationTestData))]
+        public async Task StartAutomaticComputationAsync_WithoutConstraint_SteppedRaised(StartComputationArguments<int, char> arguments)
+        {
+            var machine = new SingleTapeMachine<int, char>(arguments.TransitionTable);
+
+            var raisedStepped = await Assert.RaisesAsync<SteppedEventArgs<int, char>>(
+                handler => machine.Stepped += handler,
+                handler => machine.Stepped -= handler,
+                () => machine.StartAutomaticComputationAsync(arguments.Input));
+
+            Assert.Same(machine, raisedStepped.Sender);
+            Assert.Equal(State<int>.Accept, raisedStepped.Arguments.Transition.Range.State);
+        }
+
+        [Theory]
+        [ClassData(typeof(AcceptTerminationTestData))]
+        public async Task StartAutomaticComputationAsync_WithoutConstraint_ComputationTerminatedRaised(StartComputationArguments<int, char> arguments)
+        {
+            var machine = new SingleTapeMachine<int, char>(arguments.TransitionTable);
+
+            var raisedStepped = await Assert.RaisesAsync<ComputationTerminatedEventArgs<int, char>>(
+                handler => machine.ComputationTerminated += handler,
+                handler => machine.ComputationTerminated -= handler,
+                () => machine.StartAutomaticComputationAsync(arguments.Input));
 
             Assert.Same(machine, raisedStepped.Sender);
             Assert.Equal(State<int>.Accept, raisedStepped.Arguments.State);
